fb65c044e5670cfad86c98621ae4c4506ca251f4
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-01-31 14:25:28 -0300

    Add component "PILL"

---
+++ src/components/Pill/Pill.tsx
@@ -0,0 +1,28 @@
+import '../../index.css'
+import React from "react";
+import { cva, VariantProps } from "class-variance-authority";
+
+const pillVariants = cva(["rounded-[91px] py-[4px] px-[8px] max-w-fit max-h-fit font-Inter text-[12px] leading-[15px] text-left "], {
+    variants: {
+        variant: {
+            default: [
+                "bg-gray-400 text-white font-[400]"
+            ],
+            gradient: [
+                "bg-gradient text-black font-[600]"
+            ],
+            error: [
+                "bg-error-500 text-white font-[600]"
+            ],
+        },
+    },
+    defaultVariants: {
+        variant: "default",
+    },
+});
+
+export interface PillProps extends VariantProps<typeof pillVariants>, React.HTMLAttributes<HTMLDivElement> {}
+
+export const Pill: React.FC<PillProps> = ({ className, variant, children, ...props }) => {
+    return <div className={pillVariants({ variant , className })} {...props}>{children}</div>;
+};
