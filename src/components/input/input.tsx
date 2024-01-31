c91b0a1b5cbf6726164acb2c45462526d3a56c2f
FedericoMartucci <federicomartucci@hotmail.com>
2024-01-31 19:02:28 -0300

    feat: design cva input variants

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,11 +1,30 @@
 import { ChangeEvent, useState } from "react";
+import config from "../../../tailwind.config.js";
+import { cva, VariantProps } from "class-variance-authority";
 
-enum InputType {
-    PASSWORD = "password",
-    TEXT = "text",
-}
+const colors = config.theme.extend.colors;
 
-interface InputProps {
+const inputVariants = cva(
+  [
+    "outline-none placeholder-gray-300 bg-transparent border rounded-lg py-3 px-4 w-[306px] h-[43px] text-gray-300",
+  ],
+  {
+    variants: {
+      variant: {
+        default: ["text-white cursor-pointer", "hover:border-2 border-gray-300", "focus:border-primary-400"],
+        error: ["border-error-500"],
+        disabled: ["bg-gray-400", "cursor-not-allowed"],
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+export interface InputProps
+  extends VariantProps<typeof inputVariants>,
+    React.HTMLAttributes<HTMLInputElement> {
   value?: string;
   type?: InputType;
   helpertext?: string;
@@ -15,43 +34,38 @@ interface InputProps {
   placeholder?: string;
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
 }
+enum InputType {
+  PASSWORD = "password",
+  TEXT = "text",
+}
 
 const Input = ({
-    value = "",
-    type = InputType.TEXT,
-    helpertext = "",
-    icon = false,
-    label = "",
-    required = false,
-    onChange,
-    placeholder = "",
+  className,
+  variant,
+  value = "",
+  type = InputType.TEXT,
+  helpertext = "",
+  icon = false,
+  label = "",
+  required = false,
+  onChange,
+  placeholder = "",
 }: InputProps) => {
-  const [focus, setFocus] = useState(false);
-
   return (
     <div className="bg-gray-600 w-full h-screen flex justify-center items-center">
-     {
-        label !== "" &&
-        <label>
-            {label}
-        </label>
-     } 
+      {label !== "" && <label>{label}</label>}
       <input
-      className="border rounded-lg py-3 px-4 w-[306px] h-[43px]"
+        className={inputVariants({ variant, className })}
         value={value}
         type={type}
         required={required}
-        placeholder={placeholder}
+        placeholder="This input is quite long"
         onChange={onChange}
+        disabled={variant === "disabled"}
       />
-      {
-        helpertext !== "" &&
-        <label>
-            {helpertext}
-        </label>
-     } 
+      {helpertext !== "" && <label>{helpertext}</label>}
     </div>
   );
 };
 
-export default Input
+export default Input;
