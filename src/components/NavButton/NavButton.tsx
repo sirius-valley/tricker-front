3f1b9d24ed6e567a9e40360a71eaf3e99369efca
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-01 14:07:31 -0300

    feat: design nav buttons with cva.

---
+++ src/components/NavButton/NavButton.tsx
@@ -0,0 +1,70 @@
+import "../../index.css";
+import React from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import config from "../../../tailwind.config.js";
+import StatsIcon from "../../utils/icons/StatsIcon.js";
+import HomeIcon from "../../utils/icons/HomeIcon.js";
+import TeamIcon from "../../utils/icons/TeamIcon.js";
+import FolderIcon from "../../utils/icons/FolderIcon.js";
+import { ProfileButton } from "../ProfileButton/ProfileButton.js";
+
+const colors = config.theme.extend.colors;
+
+const navButtonVariants = cva(
+  ["flex p-3 gap-2.5 rounded-lg w-14 h-14 items-center"],
+  {
+    variants: {
+      state: {
+        on: ["bg-white/10"],
+        off: ["bg-transparent"],
+      },
+    },
+    defaultVariants: {
+      state: "off",
+    },
+  }
+);
+const iconVariant = (
+  variant: string,
+  profilePicture?: string
+): React.ReactElement => {
+  switch (variant) {
+    case "profile":
+      return <ProfileButton img={profilePicture ?? ""} />;
+    case "projects":
+      return <FolderIcon width="32px" height="32px" />;
+    case "home":
+      return <HomeIcon width="32px" height="32px" />;
+    case "team":
+      return <TeamIcon width="32px" height="32px" />;
+    case "stats":
+      return <StatsIcon width="32px" height="32px" />;
+    default:
+      return <></>;
+  }
+};
+
+export interface NavButtonProps
+  extends VariantProps<typeof navButtonVariants>,
+    React.HTMLAttributes<HTMLButtonElement> {
+  variant: string;
+  /*Temporarily, we will pass the profile picture as a string; in the future, 
+  it will be retrieved from the same component.*/
+  profilePicture?: string;
+}
+
+export const NavButton: React.FC<NavButtonProps> = ({
+  className,
+  variant,
+  profilePicture,
+  state,
+  ...props
+}) => {
+  return (
+    <button className={navButtonVariants({ state, className })} {...props}>
+      {iconVariant(variant)}
+    </button>
+  );
+};
+
+export default NavButton;
