595e021b8f3628a35afdf24f965b0547bdd40f2d
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-01 00:47:08 -0300

    feat: create stories for each squared icon.

---
+++ src/components/SquaredIconButton/SquaredIconButton.stories.tsx
@@ -0,0 +1,37 @@
+import { Meta, StoryObj } from "@storybook/react";
+import SquaredIconButton from "./SquaredIconButton";
+import PlayIcon from "../../utils/icons/PlayIcon";
+import StopIcon from "../../utils/icons/StopIcon";
+
+const meta: Meta<typeof SquaredIconButton> = {
+  title: "Components/SquaredIconButton",
+  component: SquaredIconButton,
+  tags: ["autodocs"],
+  argTypes: {
+    icon: {
+      control: {
+        type: null,
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SquaredIconButton>;
+
+export const Play: Story = {
+  tags: ["autodocs"],
+  args: {
+    icon: <PlayIcon fillColor="white" />,
+  },
+  render: (args) => <SquaredIconButton {...args} />,
+};
+
+export const TrackinTime: Story = {
+  tags: ["autodocs"],
+  args: {
+    icon: <StopIcon fillColor="white" />,
+  },
+  render: (args) => <SquaredIconButton {...args} />,
+};
