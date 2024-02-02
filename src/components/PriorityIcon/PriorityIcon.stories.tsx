89477c12b1ed65947d3872a2f0bdadc342bbff05
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-02-02 00:09:25 -0300

    Add PriorityIcon component and its stories

---
+++ src/components/PriorityIcon/PriorityIcon.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryObj } from "@storybook/react";
+import PriorityIcon from "./PriorityIcon";
+
+const meta: Meta<typeof PriorityIcon> = {
+  title: "Components/PriorityIcon",
+  component: PriorityIcon,
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: {
+        type: null,
+      },
+      options: ["feature", "improvement", "bug"],
+      table: {
+        type: {
+          summary: "feature | improvement | bug",
+        },
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof PriorityIcon>;
+
+export const PriorityIcons: Story = {
+  tags: ["autodocs"],
+  render: () => (
+    <div className="flex gap-2">
+      <PriorityIcon variant="feature" />
+      <PriorityIcon variant="improvement" />
+      <PriorityIcon variant="bug" />
+    </div>
+  )
+};
