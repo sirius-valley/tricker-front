16ed453dc2b1ca9569e7d3180424225bfb473cfb
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-06 09:05:15 -0300

    fix: add alias for absolute import references in vite.config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@components',
+        replacement: path.resolve(path.join(__dirname, '/src/components')),
+      },
+    ]
+  }
 })
