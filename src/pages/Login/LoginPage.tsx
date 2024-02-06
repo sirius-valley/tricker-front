50d74eec55424680e7e4ab69cb8a035d66786d1e
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-06 19:20:08 -0300

    feat: style LoginPage.

---
+++ src/pages/Login/LoginPage.tsx
@@ -0,0 +1,36 @@
+import LoginBackground from '@assets/Login/LoginBackground'
+import LoginImage from '@assets/Login/LoginImage.png'
+import TrickerLogo from '@assets/TrickerLogo'
+import TrickerTitle from '@assets/TrickerTitle'
+
+import { LoginWithButton } from '@components/LoginWithButton/LoginWithButton'
+import { NeedHelpButton } from '@components/NeedHelpButton/NeedHelpButton'
+
+const LoginPage = () => {
+  return (
+    <div className="flex bg-gray-700">
+      <div className='flex justify-center w-[454px] h-screen items-end'>
+        <div className="mb-[54px] flex flex-col w-[210px] h-[59%] items-center justify-between">
+          <div className="flex flex-col items-center gap-6">
+            <TrickerLogo />
+            <TrickerTitle />
+            <LoginWithButton
+              title="Log In With Google"
+              iconName="GoogleIcon"
+              redirectUrl="https://www.google.com/"
+            />
+          </div>
+          <NeedHelpButton />
+        </div>
+      </div>
+      <LoginBackground className="w-screen h-screen" />
+      <img
+        src={LoginImage}
+        className="absolute  h-[634px] bottom-0 right-0"
+        alt="Login image"
+      />
+    </div>
+  )
+}
+
+export default LoginPage
