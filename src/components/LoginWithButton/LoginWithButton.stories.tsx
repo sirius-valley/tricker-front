907cae7feacfa51bb1137178350237a35015d27c
ron2452 <87399661+ron2452@users.noreply.github.com>
2024-02-05 21:58:57 -0300

    Refactor LoginWithButton component and stories

--- src/components/LoginWithButton/LoginWithButton.stories.tsx
+++ src/components/LoginWithButton/LoginWithButton.stories.tsx
@@ -1,46 +1,46 @@
-import { Meta, StoryObj } from "@storybook/react";
-import { LoginWithButton } from "./LoginWithButton";
+import { type Meta, type StoryObj } from '@storybook/react'
+import { LoginWithButton } from './LoginWithButton'
 
 const meta: Meta<typeof LoginWithButton> = {
-  title: "Components/LoginWithButton",
+  title: 'Components/LoginWithButton',
   component: LoginWithButton,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   argTypes: {
     title: {
-      description: "The variant of the button.",
+      description: 'The variant of the button.',
       control: {
-        type: "text",
-      },
+        type: 'text'
+      }
     },
     iconName: {
       control: {
-        type: "select",
+        type: 'select'
       },
-      options: ["GoogleIcon"],
+      options: ['GoogleIcon'],
       table: {
         type: null
       },
-      description: "The icon to display on the button.",
+      description: 'The icon to display on the button.'
     },
     redirectUrl: {
-      description: "The URL to redirect to.",
+      description: 'The URL to redirect to.',
       control: {
-        type: "text",
-      },
-    },
-  },
-};
+        type: 'text'
+      }
+    }
+  }
+}
 
-export default meta;
+export default meta
 
-type Story = StoryObj<typeof LoginWithButton>;
+type Story = StoryObj<typeof LoginWithButton>
 
 export const Primary: Story = {
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   args: {
-    title: "Log In With Google",
-    iconName: "GoogleIcon",
-    redirectUrl: "https://www.google.com/",
+    title: 'Log In With Google',
+    iconName: 'GoogleIcon',
+    redirectUrl: 'https://www.google.com/'
   },
-  render: (args) => <LoginWithButton {...args} />,
-};
+  render: (args) => <LoginWithButton {...args} />
+}
