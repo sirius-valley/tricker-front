ede1554f3385340ef24f0fdea8579c9398f69246
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-06 23:23:24 -0300

    feat: change the way of setting the background image.

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -7,9 +7,7 @@ import { NeedHelpButton } from '@components/NeedHelpButton/NeedHelpButton'
 
 const LoginPage = (): JSX.Element => {
   return (
-    <div
-      className={`flex bg-login bg-cover`}
-    >
+    <div className={`flex bg-login bg-cover`}>
       <div className="flex justify-center w-[542px] h-screen items-end bg-gray-700">
         <div className="mb-[54px] flex flex-col w-[210px] h-[59%] items-center justify-between">
           <div className="flex flex-col items-center gap-6">
