a270ccd4bd0eb0dbedbf5423c23956fb8bdb0be1
Kevin Flores <kevnicflores96@gmail.com>
2024-02-07 12:26:37 -0300

    Resolve merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '../../../../../../vite.svg'
 import './App.css'
 
-// function App() {
 const App: React.FC = () => {
   const [count, setCount] = useState(0)
 
