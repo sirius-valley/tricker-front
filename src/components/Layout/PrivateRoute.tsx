f8d7e1c074c0eb088c6a41fcaeff2549c4c92efa
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-08 18:48:10 -0300

    feat: setup private routes logic to check token validation.

--- src/components/Layout/PrivateRoute.tsx
+++ src/components/Layout/PrivateRoute.tsx
@@ -1,27 +1,38 @@
+import { getToken } from '@service/Cookies'
+import { service } from '@service/service'
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRoute = (): JSX.Element => {
-  // TODO: Use authentication token
-  // const httpRequestService = useHttpRequestService()
-  const [isLoading, setisLoading] = React.useState<boolean>(true)
-  // const [isAuthorized, setIsAuthorized] = React.useState<boolean>(false)
-
-  const handlePrivateRoutes = /* async */ (): void /* Promise<void> */ => {
-    // const auth = await httpRequestService.routingAuth()
-    // setIsAuthorized(auth.isValidToken)
-    setisLoading(false)
-  }
+  const [isLoading, setIsLoading] = React.useState<boolean>(true)
+  const [isAuthorized, setIsAuthorized] = React.useState<boolean>(false)
+  const token: string = getToken()
 
   React.useEffect(() => {
-    handlePrivateRoutes()
-  }, [])
+    if (token) {
+      service
+        .me()
+        .then((res) => {
+          setIsAuthorized(res !== null)
+          return res
+        })
+        .catch((e) => {
+          console.error(e)
+          setIsAuthorized(false)
+          setIsLoading(false)
+          throw e
+        })
+    } else {
+      setIsAuthorized(false)
+    }
+    setIsLoading(false)
+  }, [token])
 
   if (isLoading) {
     // return <Loader/>
   }
 
-  return isLoading ? <Outlet /> : <Navigate to="/login" replace />
+  return isAuthorized ? <Outlet /> : <Navigate to="/login" replace />
 }
 
 export default PrivateRoute
