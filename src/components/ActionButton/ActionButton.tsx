46a20fb1861d35e9444c02a1dd6a2ceadb39b35e
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-09 13:21:27 -0300

    feat: add onClick prop to manage button state.

--- src/components/ActionButton/ActionButton.tsx
+++ src/components/ActionButton/ActionButton.tsx
@@ -5,22 +5,27 @@ import Icon from '@components/Icon/Icon'
 export interface ActionButtonProps {
   variant: 'filter' | 'add'
   disabled?: boolean
+  onClick?: () => void
 }
 
 const ActionButton: React.FC<ActionButtonProps> = ({
   variant,
-  disabled = false
+  disabled = false,
+  onClick
 }): JSX.Element => {
   const colors = config.theme.extend.colors
 
   const color: string = disabled ? colors.white + '50' : 'white'
 
   return (
-    <div className={`bg-${disabled? "gray-400" : "gradient"} rounded-lg w-fit p-px`}>
+    <div
+      className={`bg-${disabled ? 'gray-400' : 'gradient'} rounded-lg w-fit p-px`}
+    >
       <div className="flex justify-center items-center bg-gray-600 rounded-lg">
         <button
           className="flex items-center justify-center p-2 gap-4 rounded-lg bg-white/5 hover:bg-gray-400/90 disabled:cursor-not-allowed disabled:hover:bg-white/5"
           disabled={disabled}
+          onClick={onClick}
         >
           <Icon
             name={variant === 'add' ? 'AddTimeIcon' : 'FilterIcon'}
