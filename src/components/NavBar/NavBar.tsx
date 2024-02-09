4a14cac1723c44d12f140992453a9ecfd01e52e0
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-09 13:33:07 -0300

    fix: profile button do not change style when it is active.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -33,12 +33,14 @@ const NavBar: React.FC<NavBarProps> = ({ isProjectManager = false }) => {
           )}
         </NavLink>
       )}
-      <NavLink to={`/`}>
-        <NavButton
-          variant="profile"
-          profilePicture="https://images.ecestaticos.com/FVdcvD11qPRi-JWDH3USTiXDmeQ=/0x0:2120x1414/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F47b%2F328%2F963%2F47b3289639713b8e80c8d682d219fba7.jpg"
-          state="off"
-        />
+      <NavLink to={`/profile`}>
+        {({ isActive }) => (
+          <NavButton
+            variant="profile"
+            profilePicture="https://images.ecestaticos.com/FVdcvD11qPRi-JWDH3USTiXDmeQ=/0x0:2120x1414/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F47b%2F328%2F963%2F47b3289639713b8e80c8d682d219fba7.jpg"
+            state={isActive ? 'on' : 'off'}
+          />
+        )}
       </NavLink>
     </div>
   )
