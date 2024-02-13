a888a4a9bfccce8ac82a14365e99665673018d93
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-13 18:45:56 -0300

    fix: change flex alignment whether the user is pm or not.

--- src/components/TicketCard/TicketCard.tsx
+++ src/components/TicketCard/TicketCard.tsx
@@ -43,10 +43,10 @@ const TicketCard: React.FC<TicketCardProps> = ({
   }
   return (
     <button
-      className={`w-[345px] h-[114px] bg-${activeColor(`white`)}/5 border border-${activeColor(`gray-400`)} py-4 px-6 gap-4 rounded-xl flex flex-col justify-center items-center`}
+      className={`w-[345px] h-[114px] bg-${activeColor(`white`)}/5 border border-${activeColor(`gray-400`)} py-4 px-6 gap-4 rounded-xl flex flex-col`}
       onClick={handleClick}
     >
-      <div className={`flex items-start gap-1`}>
+      <div className={`flex justify-start items-start gap-1`}>
         <span className={`flex flex-col text-left gap-2 w-[260px] h-[46px]`}>
           <Body2 className={`leading-[19.36px] text-${activeColor(`white`)}`}>
             {ticketId}
