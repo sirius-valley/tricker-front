a69ac4b1452e1cd828b1852d3aab28ba97a4b0b3
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-14 09:35:35 -0300

    feat(TabItem): add active style.

--- src/components/TabBar/TabItem/TabItem.tsx
+++ src/components/TabBar/TabItem/TabItem.tsx
@@ -3,19 +3,25 @@ import React from 'react'
 
 interface ProfileButtonProps {
   teamMember?: string
+  active?: boolean
   onClick?: () => void
 }
 
 export const ProfileButton: React.FC<ProfileButtonProps> = ({
   teamMember,
+  active = false,
   onClick
 }) => {
   return (
     <button
       onClick={onClick}
-      className="w-fit h-fit border-s border-white py-2 px-4 gap-2"
+      className={`w-fit h-fit border-s border-${active ? 'primary-400' : 'white'} py-2 px-4 gap-2`}
     >
-      <Body1 className={`leading-[19.36px]`}>{teamMember}</Body1>
+      <Body1
+        className={`leading-[19.36px] text-white ${active && 'font-medium'}`}
+      >
+        {teamMember}
+      </Body1>
     </button>
   )
 }
