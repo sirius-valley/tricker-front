2c71f40f47e848436371e720bb91f80a15b215ec
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-02-15 15:02:10 -0300

    Add @tanstack/react-query and @tanstack/eslint-plugin-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { Layout } from '@components/Layout/Layout'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
 
+const queryClient = new QueryClient()
+
 function App(): JSX.Element {
-  return <Layout />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Layout />
+    </QueryClientProvider>
+  )
 }
 
 export default App
