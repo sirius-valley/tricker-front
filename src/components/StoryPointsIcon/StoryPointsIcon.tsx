51a81095d65f468bbcfeec2bf59a6cdf0250e144
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-14 23:49:16 -0300

    feat: create StoryPoints stories.

--- src/components/StoryPointsIcon/StoryPointsIcon.tsx
+++ src/components/StoryPointsIcon/StoryPointsIcon.tsx
@@ -12,7 +12,7 @@ const StoryPointsIcon: React.FC<StoryPointsIconProps> = ({
 }) => {
   return (
     <div
-      className={`border-${fillColor} border py-1 px-2 gap-2.5 rounded-full w-5 h-5`}
+      className={`flex justify-center items-center border-${fillColor} border py-1 px-2 gap-2.5 rounded-full w-5 h-5`}
     >
       <Body2
         className={`font-semibold text-xs leading-[14.52px] text-${fillColor}`}
