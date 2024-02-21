2d8f2e3c08dc09b5eaf1e38c2e75f886eccb8b69
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-02-21 16:19:44 -0300

    Add Icon component to Tooltip and update props

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import { Tooltip as ReactTooltip } from 'react-tooltip'
 import { v4 as uuidv4 } from 'uuid'
 import config from '../../../tailwind.config'
+import Icon from '@components/Icon/Icon'
+import type * as icons from '@components/Icon/index.ts'
 
 export interface TooltipProps {
-  children: React.ReactNode
+  iconName?: keyof typeof icons
+  iconWidth?: string
+  iconHeight?: string
   content: string
 }
 
-export const Tooltip: React.FC<TooltipProps> = ({ children, content }) => {
+export const Tooltip: React.FC<TooltipProps> = ({
+  iconName,
+  content,
+  iconWidth,
+  iconHeight
+}) => {
   const tooltipId = uuidv4()
   const colors = config.theme.extend.colors
 
@@ -17,9 +26,13 @@ export const Tooltip: React.FC<TooltipProps> = ({ children, content }) => {
       <a
         data-tooltip-id={tooltipId}
         data-tooltip-content={content}
-        className="flex w-fit"
+        className="flex w-fit cursor-pointer"
       >
-        {children}
+        <Icon
+          name={iconName || 'QuestionIcon'}
+          width={iconWidth}
+          height={iconHeight}
+        />
       </a>
       <ReactTooltip
         id={tooltipId}
