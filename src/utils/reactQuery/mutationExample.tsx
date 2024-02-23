adcbbbca37251ef739afb75cecf8725e095939d4
ron2452 <87399661+ron2452@users.noreply.github.com>
2024-02-23 15:02:33 -0300

    Add MutationExample component for React Query

---
+++ src/utils/reactQuery/mutationExample.tsx
@@ -0,0 +1,47 @@
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+export const MutationExample = () => {
+
+    // useMutation receives a mutationFn and returns an object with isPending, isError, error, isSuccess, reset and mutate properties
+    const { isPending, isError, error, isSuccess, reset, mutate } = useMutation({ //reset is used to reset the state of isPending, isError, error and isSuccess
+        mutationFn: (data: Post) => {
+            return axios.post('apiUrl', data)
+        },
+    })
+
+    return (
+        <div>
+            {isPending ? (
+                'Adding todo...'
+            ) : (
+                <>
+                    {isError ? (
+                        <div>An error occurred: {error.message}</div>
+                    ) : null}
+
+                    {isSuccess ? (
+                        <div>
+                            Data added successfully
+                            <button onClick={reset}>x</button>
+                        </div>) : null}
+
+                    <button
+                        onClick={() => {
+                            mutate({ userId: 1, id: 1, title: 'title', body: 'body' })
+                        }}
+                    >
+                        Create Todo
+                    </button>
+                </>
+            )}
+        </div>
+    )
+};
