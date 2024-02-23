f3aedc9eae096333ed5df5e6f64ac264b91c7434
ron2452 <87399661+ron2452@users.noreply.github.com>
2024-02-23 15:02:55 -0300

    Refactor react-query mutation example

--- src/utils/reactQuery/mutationExample.tsx
+++ src/utils/reactQuery/mutationExample.tsx
@@ -1,47 +1,46 @@
-import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
 
 interface Post {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
+  userId: number
+  id: number
+  title: string
+  body: string
 }
 
-export const MutationExample = () => {
+export const MutationExample = (): React.ReactElement => {
+  // useMutation receives a mutationFn and returns an object with isPending, isError, error, isSuccess, reset and mutate properties
+  const { isPending, isError, error, isSuccess, reset, mutate } = useMutation({
+    // reset is used to reset the state of isPending, isError, error and isSuccess
+    mutationFn: async (data: Post) => {
+      return await axios.post('apiUrl', data)
+    }
+  })
 
-    // useMutation receives a mutationFn and returns an object with isPending, isError, error, isSuccess, reset and mutate properties
-    const { isPending, isError, error, isSuccess, reset, mutate } = useMutation({ //reset is used to reset the state of isPending, isError, error and isSuccess
-        mutationFn: (data: Post) => {
-            return axios.post('apiUrl', data)
-        },
-    })
+  return (
+    <div>
+      {isPending ? (
+        'Adding todo...'
+      ) : (
+        <>
+          {isError ? <div>An error occurred: {error.message}</div> : null}
 
-    return (
-        <div>
-            {isPending ? (
-                'Adding todo...'
-            ) : (
-                <>
-                    {isError ? (
-                        <div>An error occurred: {error.message}</div>
-                    ) : null}
+          {isSuccess ? (
+            <div>
+              Data added successfully
+              <button onClick={reset}>x</button>
+            </div>
+          ) : null}
 
-                    {isSuccess ? (
-                        <div>
-                            Data added successfully
-                            <button onClick={reset}>x</button>
-                        </div>) : null}
-
-                    <button
-                        onClick={() => {
-                            mutate({ userId: 1, id: 1, title: 'title', body: 'body' })
-                        }}
-                    >
-                        Create Todo
-                    </button>
-                </>
-            )}
-        </div>
-    )
-};
+          <button
+            onClick={() => {
+              mutate({ userId: 1, id: 1, title: 'title', body: 'body' })
+            }}
+          >
+            Create Todo
+          </button>
+        </>
+      )}
+    </div>
+  )
+}
