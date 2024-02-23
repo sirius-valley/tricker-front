fab0492271f958cc784873917bff384619306f27
Kevin Flores <kevnicflores96@gmail.com>
2024-02-23 13:32:10 -0300

    Wrapper page fix

--- src/pages/Wrapper/WrapperPage.tsx
+++ src/pages/Wrapper/WrapperPage.tsx
@@ -12,13 +12,11 @@ const WrapperPage: React.FC<WrapperProps> = ({ children }) => {
       <div className="hidden md:flex flex-col items-center justify-center mt-8 mb-8">
         <TrickerLogo width="47px" height="47px" />
         <div className="mt-2">
-          <TrickerTitle width="157px"/>
+          <TrickerTitle width="157px" />
         </div>
       </div>
       <div className="w-full h-full flex-1 flex items-center justify-center">
-        <div className="mx-8 my-8">
-            {children}
-        </div>
+        <div className="mx-8 my-8">{children}</div>
       </div>
     </div>
   )
