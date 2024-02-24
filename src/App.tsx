dec70acc18bcad651500f85e4dd497be6d2a56c0
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-02-24 00:51:17 -0300

    Refactor App component to remove Redux dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { Layout } from '@components/Layout/Layout'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
-import { Provider } from 'react-redux'
-import { store } from '@redux/store'
 
 const queryClient = new QueryClient()
 
 function App(): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <Layout />
-      </Provider>
+      <Layout />
     </QueryClientProvider>
   )
 }
