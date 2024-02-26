c483ca46d65101302c2149f739c15c768e326d4c
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-02-26 18:22:36 -0300

    Add query functions for user data retrieval

---
+++ src/data-provider/query.ts
@@ -0,0 +1,42 @@
+import { useQuery } from '@tanstack/react-query'
+import * as ApiService from './service'
+import { type User, type CognitoResponse } from '@utils/types'
+
+// Hook para obtener todos los todos
+export const useGetMe = (): {
+  data: User | null | undefined
+  error: Error | null
+  isLoading: boolean
+} => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['me'],
+    queryFn: ApiService.me
+  })
+  return { data, error, isLoading }
+}
+
+export const useGetOrCreateUser = (): {
+  data: User | null | undefined
+  error: Error | null
+  isLoading: boolean
+} => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['getOrCreateUser'],
+    queryFn: ApiService.getOrCreateUser
+  })
+  return { data, error, isLoading }
+}
+
+export const useVerifyToken = (
+  code: string
+): {
+  data: CognitoResponse | null | undefined
+  error: Error | null
+  isLoading: boolean
+} => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['verifyToken', code],
+    queryFn: async () => await ApiService.verifyToken(code)
+  })
+  return { data, error, isLoading }
+}
