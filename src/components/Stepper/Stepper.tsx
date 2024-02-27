abb277ec20f5aa0cb463d3e68ec508c817be5ce1
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-27 15:16:08 -0300

    fix(Stepper): change tailwind styles by inline styling.

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -3,6 +3,7 @@ import Body2 from '@utils/typography/body2/body2'
 import { type Screen, type Step } from '@utils/types'
 import Body1 from '@utils/typography/body1/body1'
 import useScreenSize from '@hooks/useScreenSize'
+import config from '../../../tailwind.config'
 
 export interface StepperProps extends React.HTMLAttributes<HTMLInputElement> {
   currentStep: number
@@ -11,20 +12,23 @@ export interface StepperProps extends React.HTMLAttributes<HTMLInputElement> {
 
 export const Stepper: React.FC<StepperProps> = ({ currentStep, label }) => {
   const screen: Screen = useScreenSize()
+  const colors = config.theme.extend.colors
   const activeColor = (index: number): string =>
-    currentStep >= index ? 'primary-200' : 'gray-200'
+    currentStep >= index ? colors.primary[200] : colors.gray[200]
   return (
     <div className="flex w-fit">
       {label.map((step: Step, index: number) => (
         <React.Fragment key={index}>
           {index !== 0 && (
             <div
-              className={`w-[120px] h-0 mt-4 border border-${activeColor(index)}`}
+              className="w-[120px] h-0 mt-4"
+              style={{ border: `1px solid ${activeColor(index)}` }}
             />
           )}
           <div className="flex flex-col items-center gap-2.5 w-8">
             <div
-              className={`rounded-full bg-${activeColor(index)} w-8 h-8 flex justify-center items-center`}
+              className="rounded-full w-8 h-8 flex justify-center items-center"
+              style={{ backgroundColor: `${activeColor(index)}` }}
             >
               <Body2
                 className={`leading-[19.36px] text-${currentStep >= index ? 'primary-700 font-bold' : 'gray-400 font-medium'}`}
