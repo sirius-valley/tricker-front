cde666fd17baabdab347c36f53d75ea48e1abfb1
Kevin Flores <kevnicflores96@gmail.com>
2024-02-27 11:51:27 -0300

    ModalRemove component add

---
+++ src/components/ModalRemoval/ModalRemove.stories.tsx
@@ -0,0 +1,23 @@
+// import React from 'react';
+import { type Story, type Meta } from '@storybook/react';
+import ModalRemove, {ModalRemoveProps} from './ModalRemove';
+
+const meta: Meta<typeof ModalRemove> = {
+  title: 'Components/ModalRemove',
+  component: ModalRemove,
+  tags: ['autodocs'],
+  argTypes: {
+    onRemove: { action: 'Member removed' },
+  },
+};
+
+export default meta;
+
+const Template: Story<ModalRemoveProps> = (args) => <ModalRemove {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  memberName: 'Team memberName 1',
+  projectName: 'Tricker Project',
+  onRemove: () => console.log('Member removed'),
+};
