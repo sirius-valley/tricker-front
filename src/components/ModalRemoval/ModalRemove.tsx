9dd5ab83d91857299b65af05bb56f1a6489a8b7a
Kevin Flores <kevnicflores96@gmail.com>
2024-02-27 14:15:29 -0300

    Modal Remove fix

--- src/components/ModalRemoval/ModalRemove.tsx
+++ src/components/ModalRemoval/ModalRemove.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Modal } from '@components/Modal/Modal'
 import Button from '@components/Button/Button'
 import Icon from '@components/Icon/Icon'
+import Body2 from '@utils/typography/body2/body2'
 
 export interface ModalRemoveProps {
   memberName: string
@@ -26,9 +27,9 @@ const ModalRemove: React.FC<ModalRemoveProps> = ({
           onClose()
         }}
       >
-        <div className="w-[539px] min-h-[199px] bg-gray-500 px-8 py-6 m-4 rounded-lg shadow-lg text-white">
-          <div className="flex justify-between">
-            <h2 className="text-lg font-semibold mb-4">Remove Member</h2>
+        <div className="w-[539px] min-h-[199px] bg-gray-500 border border-gray-300 px-8 py-6 rounded-xl shadow-lg text-white">
+          <div className="flex justify-between w-[475px]">
+          <h5 className="font-normal mb-2" style={{ fontSize: "24px" }}>Remove Member</h5>
             <button
               className="h-fit"
               onClick={() => {
@@ -38,14 +39,15 @@ const ModalRemove: React.FC<ModalRemoveProps> = ({
               <Icon name="DismissIcon" />
             </button>
           </div>
-          <p className="text-sm mb-4">
+          <Body2 className="text-sm font-normal mb-5">
             Are you sure you want to remove {memberName} from {projectName}?
-            This action cannot be undone.
-          </p>
-          <div className="flex w-full gap-6">
+            If you change your mind, you can grant them access later.
+          </Body2>
+          <div className="flex justify-center gap-6">
             <Button
               variant="outline"
               size={'large'}
+              className="w-[225px] h-fit"
               onClick={() => {
                 onClose()
               }}
@@ -55,6 +57,7 @@ const ModalRemove: React.FC<ModalRemoveProps> = ({
             <Button
               variant="error"
               size={'large'}
+              className="w-[225px] h-fit"
               onClick={() => {
                 onRemove()
               }}
