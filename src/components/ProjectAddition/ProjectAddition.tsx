08c1f87c10d029b354c7ca41c01348c3b47a8a40
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-28 09:26:43 -0300

    fix: change responsive design as it is in Figma.

--- src/components/ProjectAddition/ProjectAddition.tsx
+++ src/components/ProjectAddition/ProjectAddition.tsx
@@ -1,6 +1,7 @@
 import Input from '@components/Input/Input'
 import SelectInput from '@components/SelectInput/SelectInput'
-import { type Project } from '@utils/types'
+import useScreenSize from '@hooks/useScreenSize'
+import { type Screen, type Project } from '@utils/types'
 import H2 from '@utils/typography/h2/h2'
 import Subtitle from '@utils/typography/subtitle/subtitle'
 import React from 'react'
@@ -16,16 +17,17 @@ export const ProjectAddition: React.FC<ProjectAdditionProps> = ({
   handleSelectedProject,
   projects
 }): JSX.Element => {
+  const screen: Screen = useScreenSize()
   return (
-    <div className="flex flex-col items-center justify-center w-[320px] md:w-fit lg:w-[1048px] bg-gray-600 border border-primary-400 py-4 px-4 md:py-20 md:px-[140px] gap-10 rounded-xl">
+    <div className="flex flex-col items-center justify-center w-[329px] md:w-fit lg:w-[1048px] bg-gray-600 shadow-2 md:shadow-none border border-primary-400 py-10 px-6 md:py-20 md:px-[140px] gap-12 md:gap-10 rounded-xl">
       <div className="gap-2">
-        <H2 className="text-white md:leading-[41.15px] text-[20px] md:text-[34px] whitespace-nowrap md:whitespace-normal font-semibold">
+        <H2 className="text-white md:leading-[41.15px] text-[24px] md:text-[34px] whitespace-nowrap md:whitespace-normal font-semibold">
           Initial Setup
         </H2>
       </div>
       <div className="flex flex-col w-full gap-8">
         <div className="flex flex-col w-full gap-4">
-          <Subtitle className="text-white text-[14px] whitespace-pre-wrap md:text-base leading-[19.36px] text-bold md:text-semibold">
+          <Subtitle className="text-white whitespace-pre-wrap md:text-base leading-[19.36px] text-semibold">
             First, select the Project Management Tool
           </Subtitle>
           <div className="flex flex-col w-full gap-2">
@@ -41,15 +43,16 @@ export const ProjectAddition: React.FC<ProjectAdditionProps> = ({
           </div>
         </div>
         <div className="flex flex-col w-full gap-4">
-          <Subtitle className="text-white text-[14px] whitespace-pre-wrap md:text-base leading-[19.36px] text-bold md:text-semibold">
+          <Subtitle className="text-white whitespace-pre-wrap md:text-base leading-[19.36px] text-semibold">
             Second, add the user token to connect to the API
           </Subtitle>
           <Input
             handleValue={handleToken}
             label="User Token"
             required
-            tooltip="You can find this under Team Settings > My Account > API > Personal Api Keys"
-            className="md:h-[69px]"
+            tooltip={`${screen.width >= 768 ? 'You can find this under Team Settings > My Account > API > Personal Api Keys' : ''}`}
+            helpertext={`${screen.width < 768 ? 'You can find this under Team Settings > My Account > API > Personal Api Keys' : ''}`}
+            className="h-[69px]"
             placeholder="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
           />
         </div>
