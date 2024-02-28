9037b3d5aa32c314457738de7964e07179dea5a2
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-02-28 00:00:40 -0300

    Refactor SelectProjectScreen component to use useGetProjects hook

--- src/components/SelectProjectScreen/SelectProjectScreen.tsx
+++ src/components/SelectProjectScreen/SelectProjectScreen.tsx
@@ -1,60 +1,22 @@
 import '../../index.css'
 import React, { useState } from 'react'
-// import { useQuery } from '@tanstack/react-query'
-// import axios from 'axios'
 import { Tooltip } from '@components/Tooltip/Tooltip'
 import NoAvatarProject from '@components/NoAvatar/NoAvatarProject'
 import Body2 from '@utils/typography/body2/body2'
 import Body1 from '@utils/typography/body1/body1'
 import H1 from '@utils/typography/h1/h1'
 import RadioButton from '@components/RadioButton/RadioButton'
+import { useGetProjects } from '@data-provider/query'
 
 export interface SelectProjectScreenProps {
-  token: string
   handleSelection: (id: string) => void
 }
-
-const mockedData = [
-  {
-    id: '1',
-    name: 'AProject 1',
-    picture: 'https://via.placeholder.com/150'
-  },
-  {
-    id: '2',
-    name: 'BProject 2',
-    picture: ''
-  },
-  {
-    id: '3',
-    name: 'CProject 3',
-    picture: ''
-  }
-]
 
 const SelectProjectScreen: React.FC<SelectProjectScreenProps> = ({
   handleSelection
 }) => {
-  // Technical debt: data fetching
-
-  const [selectedProject, setSelectedProject] = useState(mockedData[0].id)
-
-  // const { data, isLoading, isError } = useQuery({
-  //   queryKey: ['projects', token],
-  //   queryFn: async (): Promise<Project> => {
-  //     return await axios.get('https://api.example.com/projects', {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`
-  //       }
-  //     })
-  //   }
-  // })
-
-  // useEffect(() => {
-  //   if (selectedProject) {
-  //     handleSelection(selectedProject)
-  //   }
-  // }, [handleSelection])
+  const { data, isLoading, error } = useGetProjects()
+  const [selectedProject, setSelectedProject] = useState<string | null>(null)
 
   return (
     <div className="flex flex-col max-w-[1032px] min-h-[500px] border border-primary-400 rounded-xl justify-center items-center bg-gray-600">
@@ -71,8 +33,7 @@ const SelectProjectScreen: React.FC<SelectProjectScreenProps> = ({
           />
         </div>
         <div className="border border-gray-300 py-2 rounded-[8px] max-w-[752px] w-full">
-          {/* {isLoading && <div>Loading...</div>} */}
-          {mockedData.map((project) => (
+          {data?.map((project) => (
             <div
               key={project.id}
               className="flex items-center gap-4 p-4 hover:bg-gray-500 cursor-pointer"
@@ -84,11 +45,11 @@ const SelectProjectScreen: React.FC<SelectProjectScreenProps> = ({
               <RadioButton
                 handleChecked={() => {}}
                 id={project.id}
-                selectedValue={selectedProject}
+                selectedValue={selectedProject || ''}
               />
-              {project.picture && project.picture !== '' ? (
+              {project.image && project.image !== '' ? (
                 <img
-                  src={project.picture}
+                  src={project.image}
                   alt={project.name}
                   className="w-[20px] h-[20px] rounded-sm"
                 />
