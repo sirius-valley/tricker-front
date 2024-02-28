a31b209c0eadb597371aeb566ced6201b863a91b
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-28 19:26:54 -0300

    feat: add animation and style to the snackbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { Layout } from '@components/Layout/Layout'
+// import { Layout } from '@components/Layout/Layout'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
+import { useSnackBar } from '@components/SnackBarProvider/SnackBarProvider'
 
 const queryClient = new QueryClient()
 
 function App(): JSX.Element {
+  const { showSnackBar } = useSnackBar()
+
+  const handleError = (): void => {
+    showSnackBar('El usuario no pertenece a la org', 'error')
+  }
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout />
+      <button onClick={handleError}>Show Error Snackbar</button>
     </QueryClientProvider>
   )
 }
---
+++ src/components/SnackBarProvider/SnackBarProvider.tsx
@@ -0,0 +1,66 @@
+import NotificationBadge from '@components/NotificationBadge/NotificationBadge'
+import React from 'react'
+
+interface SnackBar {
+  message: string
+  type: 'default' | 'success' | 'error' | 'warning'
+}
+
+interface SnackBarContextType {
+  showSnackBar: (
+    message: string,
+    type: 'default' | 'success' | 'error' | 'warning'
+  ) => void
+}
+
+const SnackbarContext = React.createContext<SnackBarContextType | null>(null)
+
+export const useSnackBar = (): SnackBarContextType => {
+  const context = React.useContext(SnackbarContext)
+  if (!context) {
+    throw new Error('useSnackbar must be used within a SnackbarProvider')
+  }
+  return context
+}
+
+interface SnackBarProviderProps {
+  children: React.ReactNode
+}
+
+export const SnackBarProvider: React.FC<SnackBarProviderProps> = ({
+  children
+}) => {
+  const [snackBars, setSnackBars] = React.useState<SnackBar[]>([])
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setSnackBars((prev) => prev.slice(1))
+    }, 2000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [snackBars])
+
+  const showSnackBar = (
+    message: string,
+    type: 'default' | 'success' | 'error' | 'warning'
+  ): void => {
+    setSnackBars((prev) => [...prev, { message, type }])
+  }
+
+  return (
+    <SnackbarContext.Provider value={{ showSnackBar }}>
+      {children}
+      {snackBars.map((snackBar: SnackBar, index: number) => (
+        <div
+          className="fixed bottom-5 left-5 z-50 flex justify-center items-center animate-slideInLeft"
+          key={index}
+        >
+          <NotificationBadge variant={snackBar.type}>
+            {snackBar.message}
+          </NotificationBadge>
+        </div>
+      ))}
+    </SnackbarContext.Provider>
+  )
+}
