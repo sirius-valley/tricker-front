278528727f510101a697bd8dc34b3acffb4da084
ron2452 <87399661+ron2452@users.noreply.github.com>
2024-02-28 11:11:02 -0300

    Add HelperText component and useScreenSize hook to SelectProjectScreen

--- src/components/SelectProjectScreen/SelectProjectScreen.tsx
+++ src/components/SelectProjectScreen/SelectProjectScreen.tsx
@@ -5,8 +5,10 @@ import NoAvatarProject from '@components/NoAvatar/NoAvatarProject'
 import Body2 from '@utils/typography/body2/body2'
 import Body1 from '@utils/typography/body1/body1'
 import H1 from '@utils/typography/h1/h1'
+import HelperText from '@utils/typography/helpertext/helpertext'
 import RadioButton from '@components/RadioButton/RadioButton'
 import { useGetProjects } from '@data-provider/query'
+import useScreenSize from '@hooks/useScreenSize'
 
 export interface SelectProjectScreenProps {
   handleSelection: (id: string) => void
@@ -18,6 +20,7 @@ const SelectProjectScreen: React.FC<SelectProjectScreenProps> = ({
   const { data, isLoading, error } = useGetProjects()
   console.log(data, isLoading, error)
   const [selectedProject, setSelectedProject] = useState<string | null>(null)
+  const screenSize = useScreenSize()
 
   return (
     <div className="flex flex-col max-w-[1032px] min-h-[500px] border border-primary-400 rounded-xl justify-center items-center bg-gray-600">
@@ -31,16 +34,16 @@ const SelectProjectScreen: React.FC<SelectProjectScreenProps> = ({
         )}
         {data && !isLoading && (
           <>
-            <H1 className="text-white">Select Project</H1>
+            <H1 className="text-white text-[24px] md:text-[26px]">Select Project</H1>
             <div className="flex gap-1 items-center max-w-[752px] w-full">
               <Body2 className="text-white font-semibold self-start flex">
                 Now, select the project you would like to start with
               </Body2>
-              <Tooltip
+              { screenSize.width > 700 && <Tooltip
                 iconHeight="16"
                 iconWidth="16"
                 content="If you don't see your team, the token is probably from another workspace. Change your workspace and try again."
-              />
+              />}
             </div>
             <div className="border border-gray-300 py-2 rounded-[8px] max-w-[752px] w-full">
               {data?.map((project) => (
@@ -70,6 +73,10 @@ const SelectProjectScreen: React.FC<SelectProjectScreenProps> = ({
                 </div>
               ))}
             </div>
+            { screenSize.width < 700 && <HelperText className="text-white mt-4">
+              If you don't see your team, the token is probably from another
+              workspace. Change your workspace and try again.
+            </HelperText>}
           </>
         )}
       </div>
