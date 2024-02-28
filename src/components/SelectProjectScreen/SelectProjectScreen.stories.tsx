40eadc6b8fbab10f839392cb30b8158ec3a50a7d
ron2452 <87399661+ron2452@users.noreply.github.com>
2024-02-28 10:55:30 -0300

    Refactor SelectProjectScreen.stories.tsx to use QueryClientProvider

--- src/components/SelectProjectScreen/SelectProjectScreen.stories.tsx
+++ src/components/SelectProjectScreen/SelectProjectScreen.stories.tsx
@@ -1,11 +1,17 @@
 import { type Meta, type StoryObj } from '@storybook/react'
-import SelectProjectScreen, {
-  type SelectProjectScreenProps
-} from './SelectProjectScreen'
+import SelectProjectScreen from './SelectProjectScreen'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const meta: Meta<SelectProjectScreenProps> = {
+const queryClient = new QueryClient()
+
+const meta: Meta<typeof SelectProjectScreen> = {
   title: 'Components/SelectProjectScreen',
   component: SelectProjectScreen,
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>{Story()}</QueryClientProvider>
+    )
+  ],
   tags: ['autodocs'],
   argTypes: {
     handleSelection: {
