ab145dbb43e780d7767e4aa252b794558423c250
FedericoMartucci <federicomartucci@hotmail.com>
2024-02-27 23:32:21 -0300

    feat: create team member management component and change NoAvatar props.

--- src/components/NoAvatar/NoAvatarProject.tsx
+++ src/components/NoAvatar/NoAvatarProject.tsx
@@ -3,13 +3,19 @@ import Body1 from '@utils/typography/body1/body1'
 
 export interface NoAvatarProjectProps {
   text: string
+  className?: string
 }
 
-const NoAvatarProject: React.FC<NoAvatarProjectProps> = ({ text }) => {
+const NoAvatarProject: React.FC<NoAvatarProjectProps> = ({
+  text,
+  className
+}) => {
   const firstLetter = text.charAt(0).toUpperCase()
 
   return (
-    <div className="w-5 h-5 rounded-[2px] bg-primary-200 flex items-center justify-center text-primary-700">
+    <div
+      className={`${className} w-5 h-5 rounded-[2px] bg-primary-200 flex items-center justify-center text-primary-700`}
+    >
       <Body1 className="leading-[16.31px]">{firstLetter}</Body1>
     </div>
   )
---
+++ src/components/TeamMemberManagement/TeamMemberManagement.tsx
@@ -0,0 +1,97 @@
+import ModalRemove from '@components/ModalRemove/ModalRemove'
+import NoAvatarProject from '@components/NoAvatar/NoAvatarProject'
+import { ProfileButton } from '@components/ProfileButton/ProfileButton'
+import TrashIcon from '@utils/icons/TrashIcon'
+import { type User } from '@utils/types'
+import H2 from '@utils/typography/h2/h2'
+import HelperText from '@utils/typography/helpertext/helpertext'
+import Subtitle from '@utils/typography/subtitle/subtitle'
+import React from 'react'
+
+interface TeamMemberManagementProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  handleRemainingUsers: (users: User[]) => void
+  teamMembers: User[]
+  projectName: string
+}
+
+export const TeamMemberManagement: React.FC<TeamMemberManagementProps> = ({
+  handleRemainingUsers,
+  teamMembers,
+  projectName
+}): JSX.Element => {
+  const [openModal, setOpenModal] = React.useState<boolean>(false)
+  const handleRemove = (userId: string): void => {
+    const index = teamMembers.findIndex((member: User) => member.id === userId)
+    if (index !== -1) {
+      teamMembers.splice(index, 1)
+    }
+    handleRemainingUsers(teamMembers)
+  }
+  return (
+    <div className="flex flex-col items-center justify-center w-[320px] md:w-fit lg:w-[1032px] bg-gray-600 border border-primary-400 py-4 px-4 md:py-20 md:px-[140px] gap-10 rounded-xl">
+      <div className="flex gap-2">
+        <H2 className="text-white md:leading-[41.15px] text-[20px] md:text-[34px] whitespace-nowrap md:whitespace-normal font-semibold">
+          Initial Setup
+        </H2>
+      </div>
+      <div className="flex flex-col w-full gap-2">
+        <div className="flex flex-col w-full gap-4">
+          <div className="flex flex-col w-full gap-4">
+            <Subtitle className="text-white text-[14px] whitespace-pre-wrap md:text-base leading-[19.36px] text-semibold">
+              Remove those who shouldn&apos;t have access to Tricker
+            </Subtitle>
+          </div>
+          <div className="flex flex-col w-full gap-6">
+            <div className="flex flex-col w-full gap-2">
+              {teamMembers.map((member: User) => (
+                <div
+                  className="flex items-center w-full p-2 gap-4 rounded-lg bg-gray-500"
+                  key={member.id}
+                >
+                  <div className="flex w-full gap-4">
+                    {member.profileImage ? (
+                      <ProfileButton img={member.profileImage} />
+                    ) : (
+                      <NoAvatarProject
+                        text={member.username}
+                        className="w-8 h-8"
+                      />
+                    )}
+                    <div className="flex flex-col w-full gap-1">
+                      <Subtitle className="text-white">
+                        {member.username}
+                      </Subtitle>
+                      <HelperText className="text-white">
+                        {member.username.replace(/\s+/g, '').toLowerCase()}
+                        @sirius.com.ar
+                      </HelperText>
+                    </div>
+                  </div>
+                  <div
+                    onClick={() => {
+                      setOpenModal(!openModal)
+                    }}
+                  >
+                    <TrashIcon />
+                    <ModalRemove
+                      memberName={member.username}
+                      projectName={projectName}
+                      onRemove={() => {
+                        handleRemove(member.id)
+                      }}
+                      onClose={() => {
+                        setOpenModal(false)
+                      }}
+                      show={openModal}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
