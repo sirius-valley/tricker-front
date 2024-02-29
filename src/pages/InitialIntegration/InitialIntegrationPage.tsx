54d815cf53b3a58ce068f1b7943200f7c85a4f23
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-02-29 18:28:24 -0300

    Add InitialIntegrationPage component

---
+++ src/pages/InitialIntegration/InitialIntegrationPage.tsx
@@ -0,0 +1,125 @@
+import StepNavigation, {
+  StepType
+} from '@components/NextBackButtons/NextBackButtons'
+import { Stepper } from '@components/Stepper/Stepper'
+import { useAppDispatch, useAppSelector, useSteps } from '@redux/hooks'
+import { setCurrentStep } from '@redux/user'
+import { type Step, type MemberPreIntegrated } from '@utils/types'
+import WrapperPage from '@components/Wrapper/WrapperPage'
+import { ProjectAddition } from '@components/ProjectAddition/ProjectAddition'
+import { useState } from 'react'
+import SelectProjectScreen from '@components/SelectProject/SelectProject'
+import useScreenSize from '@hooks/useScreenSize'
+import Icon from '@components/Icon/Icon'
+import { TeamMemberManagement } from '@components/TeamMemberManagement/TeamMemberManagement'
+import Button from '@components/Button/Button'
+
+const InitialIntegrationPage = (): JSX.Element => {
+  const steps: Step[] = useSteps()
+  const currentStep: number = useAppSelector((state) => state.user.currentStep)
+  const dispatch = useAppDispatch()
+  let stepType: StepType
+  if (currentStep === 0) {
+    stepType = StepType.FIRST
+  } else if (currentStep === steps.length - 1) {
+    stepType = StepType.LAST
+  } else {
+    stepType = StepType.MID
+  }
+
+  const handleBackButton = (): void => {
+    if (currentStep > 0) {
+      dispatch(setCurrentStep(currentStep - 1))
+    }
+  }
+  const handleNextButton = (): void => {
+    if (currentStep < steps.length - 1) {
+      dispatch(setCurrentStep(currentStep + 1))
+    }
+  }
+
+  const [providerKey, setProviderKey] = useState<null | string>(null)
+  const [provider, setProvider] = useState<null | string>(null)
+  const [selectedProject, setSelectedProject] = useState<null | string>(null)
+  const [teamMembers, setTeamMembers] = useState<null | MemberPreIntegrated[]>(
+    null
+  )
+
+  return (
+    <WrapperPage>
+      {useScreenSize().width < 768 && currentStep !== 0 && (
+        <button
+          className="-rotate-90 top-[32px] absolute left-6 hover:bg-gray-500 rounded-full"
+          onClick={handleBackButton}
+        >
+          <Icon name="CaretUpIcon" width="32" height="32" />
+        </button>
+      )}
+      <div className="flex items-center flex-col justify-between h-screen md:h-fit gap-8 md:pt-0 pt-[100px] pb-6">
+        <Stepper currentStep={currentStep} label={steps} />
+        {currentStep === 0 && (
+          <ProjectAddition
+            providers={['Linear']}
+            handleToken={(key) => {
+              setProviderKey(key)
+            }}
+            handleSelectedProvider={(provider) => {
+              setProvider(provider)
+            }}
+          />
+        )}
+        {currentStep === 1 && providerKey && provider && (
+          <SelectProjectScreen
+            providerKey={providerKey}
+            provider={provider}
+            handleSelection={(name: string) => {
+              setSelectedProject(name)
+            }}
+          />
+        )}
+        {currentStep === 2 && (
+          <TeamMemberManagement
+            projectName="WeCan"
+            actualUser={{
+              email: 'matiaspizzi@gmail.com',
+              name: 'Matias Pizzi',
+              profileImage: null
+            }}
+            handleRemainingUsers={(users) => {
+              setTeamMembers(users)
+            }}
+          />
+        )}
+        <div className="flex gap-0 md:gap-6">
+          <StepNavigation
+            currentStep={stepType}
+            onBack={handleBackButton}
+            onNext={handleNextButton}
+            showBackButton={useScreenSize().width >= 768}
+            nextDisabled={
+              (currentStep === 0 && (!providerKey || !provider)) ||
+              (currentStep === 1 && !selectedProject)
+            }
+          ></StepNavigation>
+          {stepType === StepType.LAST && (
+            <Button
+              variant="filled"
+              className="w-[329px] h-fit md:w-[273px] text-black"
+              onClick={() => {
+                console.log({
+                  key: providerKey,
+                  project: selectedProject,
+                  members: teamMembers
+                })
+              }}
+            >
+              Continue To Project
+            </Button>
+          )}
+        </div>
+      </div>
+    </WrapperPage>
+  )
+}
+
+export default InitialIntegrationPage
