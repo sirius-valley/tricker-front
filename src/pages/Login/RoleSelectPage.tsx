43b801b15577130268678858387ec8a130334e8b
ron2452 <87399661+ron2452@users.noreply.github.com>
2024-03-01 11:50:36 -0300

    Refactor RoleSelectPage component

--- src/pages/Login/RoleSelectPage.tsx
+++ src/pages/Login/RoleSelectPage.tsx
@@ -1,58 +1,50 @@
-import StepNavigation, {
-  StepType
-} from '@components/NextBackButtons/NextBackButtons'
 import WrapperPage from '@components/Wrapper/WrapperPage'
-import { useNavigate } from 'react-router-dom'
-import { type Screen } from '@utils/types'
+import { Link } from 'react-router-dom'
 import useScreenSize from '@hooks/useScreenSize'
 import RoleButton from '@components/RoleButton/RoleButton'
+import Icon from '@components/Icon/Icon'
+import Subtitle from '@utils/typography/subtitle/subtitle'
 
 const RoleSelectPage = (): JSX.Element => {
-  const screen: Screen = useScreenSize()
-  const navigate = useNavigate()
-  return screen.width >= 768 ? (
+  return (
     <WrapperPage>
-      <div className="flex flex-col gap-8">
-        <div className="max-w-[680px] lg:max-w-[1048px] mx-auto">
-          <div className="flex justify-center w-full gap-12 mb-12">
-            <RoleButton handleClick={() => {}}>
-              I&apos;m a Project Manager
-            </RoleButton>
-            <RoleButton handleClick={() => {}}>
-              I&apos;m a Team Member
-            </RoleButton>
-          </div>
-          <div className="mb-6">
-            <StepNavigation
-              currentStep={StepType.LAST}
-              onBack={() => {
-                navigate('/login')
-              }}
-            />
-          </div>
-        </div>
-      </div>
-    </WrapperPage>
-  ) : (
-    <WrapperPage>
-      <div className="flex flex-col items-center justify-center gap-10">
-        <h1
-          className="font-normal text-white leading-[29px] text-center"
-          style={{ fontSize: '24px' }}
-        >
+      {useScreenSize().width < 768 && (
+        <Link to="/login" className="text-white">
+          <button className="-rotate-90 top-[32px] absolute left-6 hover:bg-gray-500 rounded-full">
+            <Icon name="CaretUpIcon" width="32" height="32" />
+          </button>
+        </Link>
+      )}
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-white text-center text-2xl font-normal md:hidden flex mb-10">
           What&apos;s your role?
         </h1>
-        <div className="w-[329px] flex flex-col gap-6">
-          <RoleButton handleClick={() => {}}>
-            I&apos;m a Project Manager
-          </RoleButton>
-          <RoleButton handleClick={() => {}}>I&apos;m a Team Member</RoleButton>
-          <StepNavigation
-            currentStep={StepType.LAST}
-            onBack={() => {
-              navigate('/login')
-            }}
-          />
+        {useScreenSize().width >= 768 && (
+          <div className="flex justify-center self-start pr-2 hover:bg-gray-500 rounded-full mb-4">
+            <Link
+              to="/login"
+              className="text-white flex items-center gap-2 justify-center"
+            >
+              <button className="-rotate-90">
+                <Icon name="CaretUpIcon" width="32" height="32" />
+              </button>
+              <Subtitle>Back</Subtitle>
+            </Link>
+          </div>
+        )}
+        <div>
+          <div className="flex justify-center w-full md:gap-12 gap-6 mb-12 md:flex-row flex-col">
+            <Link to="/setup" className="text-white">
+              <RoleButton handleClick={() => {}}>
+                I&apos;m a Project Manager
+              </RoleButton>
+            </Link>
+            <Link to="/login" className="text-white">
+              <RoleButton handleClick={() => {}}>
+                I&apos;m a Team Member
+              </RoleButton>
+            </Link>
+          </div>
         </div>
       </div>
     </WrapperPage>
