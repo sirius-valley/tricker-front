8ae1f062598af06b806b44c7e2208998b733d347
ron2452 <87399661+ron2452@users.noreply.github.com>
2024-03-04 13:41:07 -0300

    Refactor PrivateRoute component and add testing code

--- src/components/Layout/PrivateRoute/PrivateRoute.tsx
+++ src/components/Layout/PrivateRoute/PrivateRoute.tsx
@@ -1,20 +1,50 @@
+// eslint-disable-next-line
 import * as service from '@data-provider/service'
 import React from 'react'
 import PageWrapper from './PrivateRouteWrapper'
 import { useNavigate } from 'react-router-dom'
-import { useAppDispatch } from '@redux/hooks'
+import { useAppDispatch, useUser } from '@redux/hooks'
 import { setUser } from '@redux/user'
+import { type User } from '@utils/types'
 
 const PrivateRoute = (): JSX.Element => {
   const [isLoading, setIsLoading] = React.useState<boolean>(true)
   const [isAuthorized, setIsAuthorized] = React.useState<boolean>(true)
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
+  // -------------------- Testing porpuses, remove later --------------------
+  const mockedUser: User = {
+    id: '1',
+    email: 'username@sirius.com.ar',
+    name: 'User Name',
+    projectsRoleAssigned: [
+      {
+        id: '1',
+        userId: '1',
+        projectId: '1',
+        user: {
+          id: '1',
+          cognitoId: '',
+          profileImage: '',
+          email: '',
+          name: ''
+        },
+        role: {
+          id: '1',
+          name: 'Project Manager',
+          users: []
+        }
+      }
+    ]
+  }
+  const user = useUser()
+  // -------------------- Testing porpuses, remove later --------------------
 
-  const handlePrivateRoutes = async (): Promise<void> => {
+  React.useEffect(() => {
+    dispatch(setUser(mockedUser)) // -------------------- Testing porpuses, remove later
     try {
-      const user = await service.getOrCreateUser()
-      if (user) {
+      // const user = await service.getOrCreateUser()
+      if (user.id !== '') {
         dispatch(setUser(user))
         if (user.projectsRoleAssigned?.length === 0) {
           navigate('/login/role')
@@ -29,11 +59,8 @@ const PrivateRoute = (): JSX.Element => {
       setIsLoading(false)
       throw e
     }
-  }
-
-  React.useEffect(() => {
-    handlePrivateRoutes()
-  })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user, dispatch, setIsLoading])
 
   return <PageWrapper isLoading={isLoading} isAuthorized={isAuthorized} />
 }
