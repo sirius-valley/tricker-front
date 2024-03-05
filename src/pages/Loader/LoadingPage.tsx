cbbc8610e69ae0ba3b8665e4ed6ebed339d3c4d1
Kevin Flores <kevnicflores96@gmail.com>
2024-03-05 13:35:51 -0300

    Remove loading and SetLoading function

--- src/pages/Loader/LoadingPage.tsx
+++ src/pages/Loader/LoadingPage.tsx
@@ -1,22 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Spinner from '@components/Spinner/Spinner'
 
 const LoadingPage: React.FC = () => {
-  const [loading, setLoading] = useState<boolean>(true)
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(true)
-    }, 2000)
-  }, [])
-
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      {loading && (
+      {
         <div className="flex items-center justify-center w-full h-full">
           <Spinner variant={'primary'} size={50} />
         </div>
-      )}
+      }
     </div>
   )
 }
