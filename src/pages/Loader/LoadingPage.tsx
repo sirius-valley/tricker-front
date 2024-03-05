7eaf2cb16d7c02cbf4f81540363e5021500ba22b
Kevin Flores <kevnicflores96@gmail.com>
2024-03-05 14:23:45 -0300

    unnecessary div & { remove

--- src/pages/Loader/LoadingPage.tsx
+++ src/pages/Loader/LoadingPage.tsx
@@ -3,12 +3,8 @@ import Spinner from '@components/Spinner/Spinner'
 
 const LoadingPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      {
-        <div className="flex items-center justify-center w-full h-full">
-          <Spinner variant={'primary'} size={50} />
-        </div>
-      }
+    <div className="flex items-center justify-center w-full h-full">
+      <Spinner variant={'primary'} size={50} />
     </div>
   )
 }
