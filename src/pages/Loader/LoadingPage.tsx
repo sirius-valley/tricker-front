6595edc131ca5d5ac5e66d71a347088547e0ea95
Kevin Flores <kevnicflores96@gmail.com>
2024-03-05 13:49:18 -0300

    h-full remove

--- src/pages/Loader/LoadingPage.tsx
+++ src/pages/Loader/LoadingPage.tsx
@@ -3,7 +3,7 @@ import Spinner from '@components/Spinner/Spinner'
 
 const LoadingPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
+    <div className="flex flex-col items-center justify-center">
       {
         <div className="flex items-center justify-center w-full h-full">
           <Spinner variant={'primary'} size={50} />
