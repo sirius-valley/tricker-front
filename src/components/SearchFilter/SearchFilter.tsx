5122749f42876414028c2559e89142cb495eae8d
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-03-07 14:21:06 -0300

    New SearchFilter component

--- src/components/SearchFilter/SearchFilter.tsx
+++ src/components/SearchFilter/SearchFilter.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import Icon from '@components/Icon/Icon'
 import type * as icons from '@components/Icon/index.ts'
 import Checkbox from '@components/Checkbox/Checkbox'
+import Body1 from '@utils/typography/body1/body1'
 
 export interface OptionAttr {
   option: string
@@ -13,20 +14,16 @@ export interface OptionAttr {
 export interface SearchButtonProps {
   statusOptions: OptionAttr[]
   priorityOptions: OptionAttr[]
-  searchIcon?: keyof typeof icons
-  priorityIcon?: keyof typeof icons
+  show: boolean
 }
 
 const SearchFilter: React.FC<SearchButtonProps> = ({
   statusOptions,
   priorityOptions,
-  searchIcon,
-  priorityIcon
+  show
 }) => {
   const [showStatusOptions, setShowStatusOptions] = useState(false)
   const [showPriorityOptions, setShowPriorityOptions] = useState(false)
-  const [searchTerm, setSearchTerm] = useState('')
-  const [searchOption, setSearchOption] = useState('Search')
   const [selectedOptions, setSelectedOptions] = useState<string[]>([])
   const filterRef = useRef<HTMLDivElement | null>(null)
   const inputRef = useRef<HTMLInputElement | null>(null)
@@ -39,7 +36,6 @@ const SearchFilter: React.FC<SearchButtonProps> = ({
       if (!isClickInside) {
         setShowStatusOptions(false)
         setShowPriorityOptions(false)
-        setSearchOption('Search')
       }
     }
 
@@ -49,12 +45,6 @@ const SearchFilter: React.FC<SearchButtonProps> = ({
       document.removeEventListener('mousedown', handleClickOutside)
     }
   }, [])
-
-  const handleSearchChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ): void => {
-    setSearchTerm(event.target.value)
-  }
 
   const handleStatusOptionSelect = (option: OptionAttr): void => {
     option.selected = !option.selected
@@ -68,9 +58,6 @@ const SearchFilter: React.FC<SearchButtonProps> = ({
         prevOptions.filter((opt) => opt !== option.option)
       )
     }
-    setShowStatusOptions(true)
-    setShowPriorityOptions(false)
-    setSearchOption('Status')
   }
 
   const handlePriorityOptionSelect = (option: OptionAttr): void => {
@@ -85,113 +72,132 @@ const SearchFilter: React.FC<SearchButtonProps> = ({
         prevOptions.filter((opt) => opt !== option.option)
       )
     }
-    setShowPriorityOptions(true)
-    setShowStatusOptions(false)
-    setSearchOption('Priority')
+  }
+
+  if (!show) {
+    return null
   }
 
   return (
-    <div
-      ref={filterRef}
-      className={`relative w-[250px] bg-gray-500 border border-gray-300 rounded`}
-      onClick={() => {
-        if (inputRef.current) {
-          inputRef.current.focus()
-        }
-      }}
-    >
-      <div className="flex items-center px-3 py-2 gap-1 border-b border-gray-300">
-        {searchIcon && (
-          <Icon name={searchIcon} width={'16px'} height={'16px'} />
-        )}
-        <input
-          ref={inputRef}
-          type="text"
-          placeholder={searchOption}
-          className="bg-transparent text-sm text-white placeholder-gray-200 focus:outline-none"
-          onChange={handleSearchChange}
-        />
+    <div className="flex flex-col">
+      <div className="flex justify-end pr-8">
+        <div className="relative top-[8px] w-4 h-4 bg-gray-600 border border-gray-400 rotate-45" />
       </div>
-      <div className="flex flex-col">
-        <button
-          className={`px-4 py-2 gap-3 text-white flex items-center rounded hover:bg-gray-400 cursor-pointer ${
-            searchOption === 'Search' ? 'flex' : 'hidden'
-          }`}
-          onClick={() => {
-            setShowStatusOptions(!showStatusOptions)
-            setShowPriorityOptions(false)
-            setSearchOption('Status')
-          }}
-        >
-          <div className="w-3 h-3 rounded-full bg-white" />
-          <span>Status</span>
-        </button>
-        <button
-          className={`px-3 py-2 gap-2 text-white hover:bg-gray-400 rounded flex items-center ${
-            searchOption === 'Search' ? 'flex' : 'hidden'
-          }`}
-          onClick={() => {
-            setShowPriorityOptions(!showPriorityOptions)
-            setShowStatusOptions(false)
-            setSearchOption('Priority')
-          }}
-        >
-          {priorityIcon && (
-            <Icon name={priorityIcon} width={'18px'} height={'18px'} />
-          )}
-          <span>Priority</span>
-        </button>
-      </div>
-
-      {showStatusOptions && (
-        <div className="relative w-full text-white bg-gray-500 border-gray-300 rounded-b">
-          {statusOptions
-            .filter((option) =>
-              option.option.toLowerCase().includes(searchTerm.toLowerCase())
-            )
-            .map((option, index) => (
-              <label key={index} className="w-full">
-                <div className="px-4 py-2 gap-3 hover:bg-gray-400 rounded flex items-center cursor-pointer">
-                  <Checkbox
-                    defaultChecked={selectedOptions.includes(option.option)}
-                    onChecked={() => {
-                      handleStatusOptionSelect(option)
-                    }}
-                  />
-                  <div
-                    className="w-3 h-3 rounded-full"
-                    style={{ backgroundColor: option.color }}
-                  />
-                  <span>{option.option}</span>
-                </div>
-              </label>
-            ))}
+      <div
+        ref={filterRef}
+        className={`relative w-[306px] bg-gray-600 border border-gray-400 rounded-xl h-fit`}
+        onClick={() => {
+          if (inputRef.current) {
+            inputRef.current.focus()
+          }
+        }}
+      >
+        <div className="flex justify-between items-center p-4 pb-2 gap-1 border-b-[1px] border-gray-400">
+          <p className="font-bold text-xl text-white">Filter by:</p>
+          <button
+            onClick={() => {
+              setSelectedOptions([])
+            }}
+            className="h-fit"
+          >
+            <Body1 className="text-primary-400">Clear filters</Body1>
+          </button>
         </div>
-      )}
-      {showPriorityOptions && (
-        <div className="relative w-full text-white bg-gray-500 border-gray-300 rounded-b">
-          {priorityOptions
-            .filter((option) =>
-              option.option.toLowerCase().includes(searchTerm.toLowerCase())
-            )
-            .map((option, index) => (
-              <label key={index} className="w-full">
-                <div className="px-4 py-2 gap-2 hover:bg-gray-400 rounded text-white flex items-center cursor-pointer">
+        <div className="flex gap-[1px] rounded-b-xl bg-gray-400 flex-col">
+          <button
+            className={`px-4 py-3 gap-3 h-[52px] text-white flex items-center bg-gray-600 hover:bg-gray-400 cursor-pointer`}
+            onClick={() => {
+              setShowStatusOptions(!showStatusOptions)
+            }}
+          >
+            <div className="w-fit">
+              <div className="w-[14px] h-[14px] rounded-full bg-white" />
+            </div>
+            <div className="flex justify-between w-full">
+              <Body1 className="font-medium leading-[19.36px]">Status</Body1>
+              <Icon
+                width="20"
+                height="20"
+                name={showStatusOptions ? 'CaretUpIcon' : 'CaretDownIcon'}
+              />
+            </div>
+          </button>
+          {showStatusOptions && (
+            <div className="flex flex-col w-full gap-[1px] text-white bg-gray-400 border-gray-400">
+              {statusOptions.map((option, index) => (
+                <label key={index} className="w-full h-11">
+                  <div className="px-5 h-full py-2 bg-gray-500 hover:bg-gray-400  flex justify-between items-center cursor-pointer">
+                    <div className="flex items-center gap-2">
+                      <div
+                        className="w-[10px] h-[10px] rounded-full"
+                        style={{ backgroundColor: option.color }}
+                      />
+                      <span>{option.option}</span>
+                    </div>
+                    <Checkbox
+                      defaultChecked={selectedOptions.includes(option.option)}
+                      onChecked={() => {
+                        handleStatusOptionSelect(option)
+                      }}
+                    />
+                  </div>
+                </label>
+              ))}
+            </div>
+          )}
+          <button
+            style={{
+              borderRadius: showPriorityOptions ? '0' : '0 0 12px 12px'
+            }}
+            className={`px-4 py-3 gap-2 h-[52px] text-white bg-gray-600 hover:bg-gray-400 flex items-center`}
+            onClick={() => {
+              setShowPriorityOptions(!showPriorityOptions)
+            }}
+          >
+            <div className="w-5 h-5">
+              <Icon name={'MediumPriorityIcon'} width="20" height="20" />
+            </div>
+            <div className="flex justify-between w-full">
+              <Body1 className="font-medium leading-[19.36px]">Priority</Body1>
+              <Icon
+                width="20"
+                height="20"
+                name={showPriorityOptions ? 'CaretUpIcon' : 'CaretDownIcon'}
+              />
+            </div>
+          </button>
+        </div>
+        {showPriorityOptions && (
+          <div className="flex flex-col w-full gap-[1px] text-white bg-gray-400 border-y-[1px] border-gray-400 rounded-b-xl">
+            {priorityOptions.map((option, index) => (
+              <label key={index} className="w-full h-11">
+                <div
+                  style={{
+                    borderRadius:
+                      index === priorityOptions.length - 1
+                        ? '0 0 12px 12px'
+                        : '0'
+                  }}
+                  className="px-5 h-full py-2 bg-gray-500 hover:bg-gray-400  flex justify-between items-center cursor-pointer"
+                >
+                  <div className="flex items-center gap-2">
+                    {option.icon && (
+                      <Icon name={option.icon} width={'18px'} height={'18px'} />
+                    )}
+                    <span>{option.option}</span>
+                  </div>
                   <Checkbox
                     defaultChecked={selectedOptions.includes(option.option)}
                     onChecked={() => {
                       handlePriorityOptionSelect(option)
                     }}
                   />
-                  {option.icon && (
-                    <Icon name={option.icon} width={'18px'} height={'18px'} />
-                  )}
-                  <span>{option.option}</span>
                 </div>
               </label>
             ))}
-        </div>
-      )}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
