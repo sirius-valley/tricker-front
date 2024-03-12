79d3687ebe89e8b401d8f824596b34cdd522a1b8
FedericoMartucci <federicomartucci@hotmail.com>
2024-03-11 23:33:03 -0300

    feat: add stories and change router.tsx to the original path.

--- src/components/Layout/Router.tsx
+++ src/components/Layout/Router.tsx
@@ -2,26 +2,15 @@ import { createBrowserRouter } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute/PrivateRoute'
 import EmptyProjectPage from '@pages/Login/EmptyProjectPage'
 import RoleSelectPage from '@pages/Login/RoleSelectPage'
-// import LoginPage from '@pages/Login/LoginPage'
 import SetupPage from '@pages/InitialIntegration/InitialIntegrationPage'
 import LoadingPage from '@pages/Loader/LoadingPage'
-import FilterSection from '@components/FilterSection/FilterSection'
 import HomeWrapperPage from '@components/HomeWrapperPage/HomeWrapperPage'
+import LoginPage from '@pages/Login/LoginPage'
 
 export const ROUTER = createBrowserRouter([
   {
     path: '/login',
-    element: (
-      <div className="h-screen w-screen bg-gray-700">
-        <FilterSection
-          handleSelect={function (): void {}}
-          handleSearch={function (): void {}}
-          handleView={function (view: 'grid' | 'list'): void {
-            console.log(view)
-          }}
-        />
-      </div>
-    ) // StepperExample
+    element: <LoginPage /> // StepperExample
   },
   {
     path: '/login/non-invited',
