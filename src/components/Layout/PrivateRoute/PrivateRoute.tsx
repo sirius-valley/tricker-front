b303e18b660dfd1d64f36a5ded36db6dfcb928d1
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-03-18 13:20:38 -0300

    import useEffect, useState

--- src/components/Layout/PrivateRoute/PrivateRoute.tsx
+++ src/components/Layout/PrivateRoute/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import PageWrapper from './PrivateRouteWrapper'
 import { useNavigate } from 'react-router-dom'
 import { useAppDispatch } from '@redux/hooks'
@@ -7,14 +7,14 @@ import { useGetOrCreateUser } from '@data-provider/query'
 import { useSnackBar } from '@components/SnackBarProvider/SnackBarProvider'
 
 const PrivateRoute = (): JSX.Element => {
-  const [isAuthorized, setIsAuthorized] = React.useState<boolean>(true)
+  const [isAuthorized, setIsAuthorized] = useState<boolean>(true)
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
   const { showSnackBar } = useSnackBar()
 
   const { data, isLoading, error } = useGetOrCreateUser()
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (error) {
       showSnackBar('You are not authorized to access this page', 'error')
       setIsAuthorized(false)
