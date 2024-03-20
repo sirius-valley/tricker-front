c0988514d97d064c0cbc2f18be37c94e0c212e23
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-03-20 10:22:16 -0300

    Add responsive rendering of NoTicketSvg based on screen size

--- src/components/NoTicketMessage/NoTicketMessage.tsx
+++ src/components/NoTicketMessage/NoTicketMessage.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 import Body2 from '@utils/typography/body2/body2'
 import Body1 from '@utils/typography/body1/body1'
 import NoTicketSvg from '../../assets/NoTicket.svg'
+import useScreenSize from '@hooks/useScreenSize'
 
 const NoTicketMessage: React.FC = (): JSX.Element => {
+  const screen = useScreenSize()
   return (
     <div className="w-fit h-fit flex flex-col justify-center items-center gap-4 text-white">
       <div className="flex flex-col gap-1">
         <img src={NoTicketSvg} alt="My SVG" />
-        <img src={NoTicketSvg} alt="My SVG" />
-        <img src={NoTicketSvg} alt="My SVG" />
+        {screen.width >= 768 && (
+          <>
+            <img src={NoTicketSvg} alt="My SVG" />
+            <img src={NoTicketSvg} alt="My SVG" />
+          </>
+        )}
       </div>
       <div className="flex flex-col items-center justify-center gap-1">
         <Body2>No assigned tasks</Body2>
