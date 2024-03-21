c45c31955600da7774ff003f7d9796c404b20769
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-03-21 13:28:03 -0300

    Add rounded bottom-left corner to TicketList component

--- src/components/TicketList/TicketList.tsx
+++ src/components/TicketList/TicketList.tsx
@@ -96,7 +96,7 @@ const TicketList: React.FC<TicketListProps> = ({
   // }
   return (
     <div
-      className={`w-[393px] md:w-[467px] h-[770px] bg-gray-500 ${data ? 'overflow-y-auto' : 'overflow-y-hidden'} scrollbar-hide`}
+      className={`w-[393px] md:w-[467px] h-[770px] bg-gray-500 ${data ? 'overflow-y-auto' : 'overflow-y-hidden'} scrollbar-hide rounded-bl-xl`}
     >
       {isLoading && (
         <div className="p-6 w-full">
