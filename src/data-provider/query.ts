e89418fd3ae462c1d6ddcaab56436be891b678bc
FedericoMartucci <federicomartucci@hotmail.com>
2024-04-04 09:06:22 -0300

    feat: create query to retrieve filters

--- src/data-provider/query.ts
+++ src/data-provider/query.ts
@@ -11,7 +11,9 @@ import type {
   IssueView,
   IssueDetail,
   ModifyTimeData,
-  IssueChronologyEvent
+  IssueChronologyEvent,
+  DevProjectFiltersDTO,
+  PMProjectFiltersDTO
 } from '@utils/types'
 
 export const useGetMe = (): {
@@ -261,6 +263,21 @@ export const useGetTicketElapsedTime = (
   const { data, error, isLoading } = useQuery({
     queryKey: ['getTicketElapsedTime', issueId],
     queryFn: async () => await ApiService.getTicketElapsedTime(issueId)
+  })
+  return { data, error, isLoading }
+}
+
+export const useGetFilters = (
+  projectId: string,
+  userRole: 'pm' | 'dev'
+): {
+  data: DevProjectFiltersDTO | PMProjectFiltersDTO | null | undefined
+  error: Error | null
+  isLoading: boolean
+} => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['getFilters', projectId, userRole],
+    queryFn: async () => await ApiService.getFilters(projectId, userRole)
   })
   return { data, error, isLoading }
 }
