247474da64799ad744d937bb1e7f3b9f7789556c
FedericoMartucci <federicomartucci@hotmail.com>
2024-04-08 15:02:52 -0300

    feat: add accept and decline icons to confirmation page

--- src/pages/EmailConfirmation/ConfirmationPage.tsx
+++ src/pages/EmailConfirmation/ConfirmationPage.tsx
@@ -1,3 +1,4 @@
+import Icon from '@components/Icon/Icon'
 import WrapperPage from '@components/Wrapper/WrapperPage'
 import Body1 from '@utils/typography/body1/body1'
 import H2 from '@utils/typography/h2/h2'
@@ -17,7 +18,7 @@ const ConfirmationPage: React.FC<ConfirmationPageProps> = ({
   return (
     <WrapperPage>
       <div className="flex flex-col md:mb-36">
-        <div className="border flex flex-col lg:w-[1000px] md:w-[750px] w-[329px] h-fit bg-gray-600 border-primary-400 p-6 gap-4 rounded-xl shadow-2 items-center justify-center">
+        <div className="border flex flex-col lg:w-[1000px] md:w-[750px] w-[329px] h-fit bg-gray-600 border-primary-400 p-6 lg:px-[136px] gap-4 rounded-xl shadow-2 items-center justify-center">
           <div className="flex gap-4 items-center justify-center">
             <div
               className={`w-20 h-20 rounded-[40px] bg-primary-200 flex items-center justify-center text-primary-700 p-2`}
@@ -32,13 +33,7 @@ const ConfirmationPage: React.FC<ConfirmationPageProps> = ({
                 }
               </Body1>
             </div>
-            <div
-              className={`w-20 h-20 rounded-[40px] bg-primary-200 flex items-center justify-center text-primary-700 p-2`}
-            >
-              <Body1 className="text-[36px] font-medium leading-[43.57px]">
-                aa
-              </Body1>
-            </div>
+            <Icon name={decline ? 'DeclineIcon' : 'AcceptIcon'} />
             <div
               className={`w-20 h-20 rounded-[40px] bg-primary-200 flex items-center justify-center text-primary-700 p-2`}
             >
@@ -53,11 +48,11 @@ const ConfirmationPage: React.FC<ConfirmationPageProps> = ({
               </Body1>
             </div>
           </div>
-          <H2 className="md:text-[34px] md:leading-[41.15px] max-w-[650px] w-fit text-xl leading-[24.2px] text-center text-white font-medium">
+          <H2 className="md:text-[34px] md:leading-[41.15px] w-fit text-xl leading-[24.2px] text-center text-white font-medium">
             You&apos;ve {decline ? 'declined' : 'accepted'} {username}&apos;s
             request to add WeCan to Tricker
           </H2>
-          <Body1 className="text-[22px] leading-[26.63px] text-center text-gray-300 font-medium">
+          <Body1 className="md:text-[22px] leading-[26.63px] text-center text-gray-300 font-medium">
             We&apos;ll notify her {!decline && 'the next steps'} via email
           </Body1>
         </div>
--- src/utils/icons/DeclineIcon.tsx
+++ src/utils/icons/DeclineIcon.tsx
@@ -5,9 +5,9 @@ const DeclineIcon = (props: IconProps): JSX.Element => {
 
   return (
     <svg
-      width={width || 24}
-      height={height || 24}
-      viewBox="0 0 24 24"
+      width={width || 27}
+      height={height || 27}
+      viewBox="0 0 26 27"
       fill={fillColor || '#FEFEFE'}
       xmlns="http://www.w3.org/2000/svg"
     >
