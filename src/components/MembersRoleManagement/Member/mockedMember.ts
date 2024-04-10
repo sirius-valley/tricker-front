9e4de949fd0b6cc8e2a10903f4ba1e05fa768a2c
FedericoMartucci <federicomartucci@hotmail.com>
2024-04-10 19:26:08 -0300

    feat: create member story

---
+++ src/components/MembersRoleManagement/Member/mockedMember.ts
@@ -0,0 +1,10 @@
+import { type TeamMember } from '@utils/types'
+
+export const mockedMember: TeamMember = {
+  email: 'federicomartucci@hotmail.com',
+  name: 'Federico Martucci',
+  role: {
+    id: '1',
+    name: 'Project Manager'
+  }
+}
