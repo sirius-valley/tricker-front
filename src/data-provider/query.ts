0522b418abfeb4baa47e2b592f6e4d8b59b59e27
Kevin Flores <kevnicflores96@gmail.com>
2024-04-11 15:33:34 -0300

    query add

--- src/data-provider/query.ts
+++ src/data-provider/query.ts
@@ -11,7 +11,8 @@ import type {
   IssueView,
   IssueDetail,
   ModifyTimeData,
-  IssueChronologyEvent
+  IssueChronologyEvent,
+  MyProjectsOption
 } from '@utils/types'
 
 export const useGetMe = (): {
@@ -283,6 +284,20 @@ export const useGetChronology = (
   const { data, error, isLoading } = useQuery({
     queryKey: ['getChronology', issueId],
     queryFn: async () => await ApiService.getChronology(issueId)
+  })
+  return { data, error, isLoading }
+}
+
+export const useGetMyProjects = (
+  userId: string
+): {
+  data: MyProjectsOption[] | undefined
+  error: Error | null
+  isLoading: boolean
+} => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['getMyProjects', userId],
+    queryFn: async () => await ApiService.getMyProjects(userId)
   })
   return { data, error, isLoading }
 }
