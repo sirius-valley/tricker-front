3366781b2926cdb7f70834a2d7b640dde313f288
FedericoMartucci <federicomartucci@hotmail.com>
2024-04-15 11:54:25 -0300

    feat: open modal when add project button is clicked

--- src/components/AddProject/AddProject.tsx
+++ src/components/AddProject/AddProject.tsx
@@ -8,6 +8,7 @@ import useDebounce from '@hooks/useDebounce'
 import Icon from '@components/Icon/Icon'
 import Body2 from '@utils/typography/body2/body2'
 import ActionButton from '@components/ActionButton/ActionButton'
+import ModalAddNewProject from '@components/ModalAddNewProject/ModalAddNewProject'
 
 export interface AddProjectProps {
   handleSearch: (searchedProject: string) => void
@@ -16,7 +17,7 @@ export interface AddProjectProps {
 const AddProject: React.FC<AddProjectProps> = ({
   handleSearch
 }: AddProjectProps): JSX.Element => {
-  //   const [showModal, setShowModal] = useState<boolean>(false)
+  const [showModal, setShowModal] = useState<boolean>(false)
   const [searchedValue, setSearchedValue] = useState<string>('')
   const screen = useScreenSize()
 
@@ -28,37 +29,57 @@ const AddProject: React.FC<AddProjectProps> = ({
     handleSearchDebounced(searchedValue)
   }, [searchedValue])
 
-  return screen.width >= 768 ? (
-    <div className="flex flex-col justify-center w-full">
-      <div className="max-w-[467px] h-fit rounded-tl-xl bg-gray-500 border-b border-white/10 flex flex-wrap items-center justify-between gap-8 p-[22px] pl-6">
-        <div className="flex w-fit gap-2 items-center">
-          <Body1 className="text-[18px] leading-[22px] text-white">
-            My Projects
-          </Body1>
-          <SquaredIconButton onClick={() => {}} icon={<AddTimeIcon />} />{' '}
-        </div>
-        <div className="flex gap-2 items-center">
-          <SearchBar handleValue={setSearchedValue} variant={'desktop'} />
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div className="flex flex-col justify-center w-full">
-      <div className="bg-gray-700 flex flex-wrap items-center justify-center gap-4 p-6 pb-4 w-full">
-        <div className="flex w-full">
-          <div className="flex w-fit gap-2 items-center">
-            <Icon name={'FolderIcon'} />
-            <Body2 className="text-[20px] leading-[22px] text-white">
-              My Projects
-            </Body2>
+  return (
+    <>
+      <ModalAddNewProject
+        onClose={() => {
+          setShowModal(false)
+        }}
+        show={showModal}
+      />
+      {screen.width >= 768 ? (
+        <div className="flex flex-col justify-center w-full">
+          <div className="max-w-[467px] h-fit rounded-tl-xl bg-gray-500 border-b border-white/10 flex flex-wrap items-center justify-between gap-8 p-[22px] pl-6">
+            <div className="flex w-fit gap-2 items-center">
+              <Body1 className="text-[18px] leading-[22px] text-white">
+                My Projects
+              </Body1>
+              <SquaredIconButton
+                onClick={() => {
+                  setShowModal(true)
+                }}
+                icon={<AddTimeIcon />}
+              />{' '}
+            </div>
+            <div className="flex gap-2 items-center">
+              <SearchBar handleValue={setSearchedValue} variant={'desktop'} />
+            </div>
           </div>
         </div>
-        <div className="flex gap-4 w-full items-center">
-          <SearchBar handleValue={setSearchedValue} variant={'mobile'} />
-          <ActionButton onClick={() => {}} variant="add" />{' '}
+      ) : (
+        <div className="flex flex-col justify-center w-full">
+          <div className="bg-gray-700 flex flex-wrap items-center justify-center gap-4 p-6 pb-4 w-full">
+            <div className="flex w-full">
+              <div className="flex w-fit gap-2 items-center">
+                <Icon name={'FolderIcon'} />
+                <Body2 className="text-[20px] leading-[22px] text-white">
+                  My Projects
+                </Body2>
+              </div>
+            </div>
+            <div className="flex gap-4 w-full items-center">
+              <SearchBar handleValue={setSearchedValue} variant={'mobile'} />
+              <ActionButton
+                onClick={() => {
+                  setShowModal(true)
+                }}
+                variant="add"
+              />{' '}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
