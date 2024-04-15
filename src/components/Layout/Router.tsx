a17b80fa0e429fc968e65084aabd4ae53df722ad
ron2452 <87399661+ron2452@users.noreply.github.com>
2024-04-15 15:43:05 -0300

    Refactor import statement in Router.tsx to use ProjectsSection instead of MyProjectSelect

--- src/components/Layout/Router.tsx
+++ src/components/Layout/Router.tsx
@@ -9,7 +9,7 @@ import LoginPage from '@pages/Login/LoginPage'
 import RoleValidation from './RoleValidation/RoleValidation'
 import TicketsSection from '@components/TicketsSection/TicketsSection'
 import LoginFlowValidation from './LoginFlowValidation/LoginFlowValidation'
-import { MyProjectSelect } from '@components/MyProjectSelection/MyProjectSelection'
+import ProjectsSection from '@components/ProjectsSection/ProjectsSection'
 
 export const ROUTER = createBrowserRouter([
   {
@@ -66,7 +66,7 @@ export const ROUTER = createBrowserRouter([
           },
           {
             path: '/projects',
-            element: <MyProjectSelect />
+            element: <ProjectsSection />
           }
         ]
       }
