2611c8b6bdb4f1dbed12d9fc546a7a0a9fc4b6b5
ron2452 <87399661+ron2452@users.noreply.github.com>
2024-04-15 15:44:19 -0300

    Refactor ProjectsSection component in ProjectsSection.tsx

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -3,15 +3,20 @@ import MembersRoleManagement from '@components/MembersRoleManagement/MembersRole
 import { useGetProject } from '@data-provider/query'
 import { useSnackBar } from '@components/SnackBarProvider/SnackBarProvider'
 import { useEffect } from 'react'
-export interface ProjectsSectionProps {
-  projectId?: string
-}
+import useScreenSize from '@hooks/useScreenSize'
+import { MyProjectSelect } from '@components/MyProjectSelection/MyProjectSelection'
+import { useSelectedProjectInfo, useAppDispatch } from '@redux/hooks'
+import { Modal } from '@components/Modal/Modal'
+import Body1 from '@utils/typography/body1/body1'
+import { setSelectedProjectInfo } from '@redux/user'
 
-const ProjectsSection: React.FC<ProjectsSectionProps> = ({
-  projectId = '97cd2474-c742-4240-b0a0-f254729f7042'
-}: ProjectsSectionProps): JSX.Element => {
-  const { data, isLoading, error } = useGetProject(projectId)
+const ProjectsSection: React.FC = (): JSX.Element => {
+  const project = useSelectedProjectInfo()
 
+  const { data, isLoading, error } = useGetProject(project.id)
+
+  const dispatch = useAppDispatch()
+  const screen = useScreenSize()
   const { showSnackBar } = useSnackBar()
 
   useEffect(() => {
@@ -23,12 +28,23 @@ const ProjectsSection: React.FC<ProjectsSectionProps> = ({
     }
   }, [error, data])
 
-  return (
-    <div className="h-full w-full flex flex-col text-white">
+  const deselectCurrentProject = (): void => {
+    dispatch(
+      setSelectedProjectInfo({
+        id: '',
+        name: '',
+        image: ''
+      })
+    )
+  }
+
+  return screen.width >= 768 ? (
+    <div className="h-full w-full flex text-white">
+      <MyProjectSelect />
       {data && !isLoading && (
-        <div className="flex flex-col max-w-[800px] w-full pt-[60px] md:px-16 px-8 self-end gap-10 overflow-y-auto">
+        <div className="flex flex-col w-full h-full pt-[60px] 2xl:px-16 px-8 self-end gap-10 overflow-y-auto overflow-x-hidden border-l border-gray-400">
           <ProjectDetails
-            id={projectId}
+            id={project.id}
             url={data.url}
             name={data.name}
             lastSync={new Date(data.updatedAt)}
@@ -37,10 +53,42 @@ const ProjectsSection: React.FC<ProjectsSectionProps> = ({
           />
           <MembersRoleManagement
             members={data.users}
-            projectId={projectId}
+            projectId={project.id}
             projectName={data.name}
           />
         </div>
+      )}
+    </div>
+  ) : (
+    <div className="h-full w-full flex text-white">
+      <MyProjectSelect />
+      {data && !isLoading && (
+        <Modal onClose={deselectCurrentProject} show={project.id !== ''}>
+          <div className="flex flex-col w-full h-full bg-gray-700 overflow-y-auto">
+            <div className="w-full items-end min-h-[82px] justify-center flex flex-col">
+              <button onClick={deselectCurrentProject}>
+                <Body1 className="text-primary-400 text-[16px] p-4">
+                  Close
+                </Body1>
+              </button>
+            </div>
+            <div className="w-full h-full flex flex-col gap-6 px-8 pb-8">
+              <ProjectDetails
+                id={project.id}
+                url={data.url}
+                name={data.name}
+                lastSync={new Date(data.updatedAt)}
+                provider={data.issueProviderName}
+                isProjectManager={true}
+              />
+              <MembersRoleManagement
+                members={data.users}
+                projectId={project.id}
+                projectName={data.name}
+              />
+            </div>
+          </div>
+        </Modal>
       )}
     </div>
   )
