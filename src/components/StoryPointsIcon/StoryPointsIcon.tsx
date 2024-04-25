7a0e32ce8336ab9494d6178a5c86a9e8fa093eae
FedericoMartucci <federicomartucci@hotmail.com>
2024-04-25 13:47:31 -0300

    feat: add animation to expand StoryPointIcon

--- src/components/StoryPointsIcon/StoryPointsIcon.tsx
+++ src/components/StoryPointsIcon/StoryPointsIcon.tsx
@@ -13,14 +13,24 @@ const StoryPointsIcon: React.FC<StoryPointsIconProps> = ({
 }) => {
   return (
     <div
-      className={`${className} flex justify-center items-center border py-1 px-2 gap-2.5 rounded-full w-5 h-5`}
-      style={{ borderColor: fillColor, color: fillColor }}
+      className={`${className} flex justify-center items-center gap-2.5 rounded-full h-5 w-5 hover:w-fit text-nowrap`}
+      style={{ color: fillColor }}
     >
-      <p
-        className={`font-semibold font-inter leading-[14.52px] text-${fillColor}`}
+      <div
+        style={{ width: '100%', height: '100%', borderColor: fillColor }}
+        className="group rounded-full border py-1 px-2 flex gap-1 items-center justify-center"
       >
-        {points}
-      </p>
+        <p
+          className={`font-semibold font-inter leading-[14.52px] text-${fillColor}`}
+        >
+          {points}
+        </p>
+        <p
+          className={`duration-500 font-semibold font-inter leading-[14.52px] text-${fillColor} hidden group-hover:block `}
+        >
+          Story Points
+        </p>
+      </div>
     </div>
   )
 }
